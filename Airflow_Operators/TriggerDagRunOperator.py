b99b44d46cf0bbdbfe2be5f1c794dba7b93e2a59
Luu Trung Hieu <102757559+hieuluu3008@users.noreply.github.com>
2025-03-31 11:46:18 +0700

    Update TriggerDagRunOperator.py

--- Airflow_Operators/TriggerDagRunOperator.py
+++ Airflow_Operators/TriggerDagRunOperator.py
@@ -1,30 +1,66 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from datetime import timedelta
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime, timedelta
+import requests,pytz,json
 
-default_arguments = {
-    'owner': 'user_name', #optional - hỗ trợ trong việc quản lý thông tin, hỗ trợ tìm kiếm trong DAG
-    'retries': 1,  # Số lần thử lại khi task thất bại
-    'retry_delay': timedelta(minutes=5),  # Thời gian chờ giữa các lần thử lại
-    'start_date': datetime(2023, 1, 1)  # Ngày bắt đầu chạy DAG
+
+#Information
+dagid = 'control_dag_name'
+PIC ="hieult"
+
+
+def task_failure_callback(context):
+    webhook_url = 'https://chat.googleapis.com/v1/spaces/XXX/messages?key=YYYY&token=ZZZZZ'
+    dag_id = context['dag'].dag_id
+    task_instance = context['task_instance']
+    execution_date = context['execution_date']
+    log_url=f"{task_instance.log_url}"
+    log_url=log_url.replace("http://localhost:8080", "https://airflow-serving.company.com")
+    message = {"text":f"""*DAG RUN FAILED:*\nDagId: {dag_id}\nTaskId: {task_instance.task_id}\nTime: {execution_date.astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))}\nLog: {log_url}"""}
+    response = requests.post(webhook_url, data=json.dumps(message), headers={"Content-Type": "application/json"})
+    if response.status_code == 200:
+        print('Message sent successfully!')
+    else:
+        print(f'Error: {response.status_code}, {response.text}')
+
+
+default_args = {
+'owner': 'BI_Team',
+'depends_on_past': False,
+'catchup':False,
+'email_on_failure': False,
+'email_on_retry': False,
+'retries': 1,
+'retry_delay': timedelta(minutes=10),
+'start_date': datetime(2024, 10, 25),
+'execution_timeout': timedelta(minutes=55),
+"on_failure_callback": task_failure_callback
 }
 
-dag = DAG(
-	dag_id = 'dag_etl', # DAG name on UI
-	default_args = default_arguments,
-	description = 'trigger dag', # description on UI
-	schedule_interval = '05 5 * * *' # thời gian chạy của DAG theo Cron
-)
 
-task_1 = TriggerDagRunOperator(
-    task_id='trigger_dag', # task name on UI
-    trigger_dag_id='dag_raw_sales',  # dag_id want to trigger
-    wait_for_completion=True # wait for the triggered dag to complete        
-)
+with DAG(
+    dag_id= dagid,
+    default_args= default_args,
+    schedule='45 8,11,14,17,22 * * *',
+    dagrun_timeout= timedelta(minutes= 120),
+    description='Control DAG',
+    catchup= False
+) as dag:
 
-task_2 = TriggerDagRunOperator(
-    task_id='trigger_dag', # task name on UI
-    trigger_dag_id='dag_etl_sales',  # dag_id want to trigger
-    wait_for_completion=True # wait for the triggered dag to complete        
-)
+
+    task1 = TriggerDagRunOperator(
+        task_id='get_raw_data',
+        trigger_dag_id='get_raw_data.py',  
+        wait_for_completion=True,                  
+    )
+
+
+    task2 = TriggerDagRunOperator(
+        task_id='etl_process',
+        trigger_dag_id='etl_process.py',  
+        wait_for_completion=True,                  
+    )
+
+
+    task1 >> task2
